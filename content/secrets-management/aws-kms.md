6ccacb1b5fc012435657a8cf6bd531725c644ad3
Igor Rodionov <goruha@gmail.com>
2018-05-21 23:30:19 +0600

    Added chamber docs

--- content/secrets-management/aws-kms.md
+++ content/secrets-management/aws-kms.md
@@ -9,11 +9,45 @@ None
 
 # Install
 
-## Using Geodesic Master Helmfile
+{{% dialog type="warning" icon="fa-info-circle" title="Prerequisites" %}}
+This assumes you've followed the [Geodesic Module Usage with Terraform]({{< relref "geodesic/module/usage/with-terraform.md" >}}) guide which covers all the scaffolding necessary to get started.
+{{% /dialog %}}
+
+## Add aws-kms terraform module
+
+Create file in `./conf/aws/kms.tf` with following content
+
+{{% include-code-block title="./conf/aws/kms.tf" file="secrets-management/examples/terraform-aws-kms.tf" language="hcl" %}}
+
+## Rebuild the module
+
+[Rebuild](/geodesic/module/usage/) the module
+```shell
+> make build
+```
+
+##  Run into the module shell
+
+Run the Geodesic shell.
+```shell
+> $CLUSTER_NAME
+> assume-role
+```
+{{% include-code-block title="Run the Geodesic Shell" file="geodesic/module/usage/examples/start-geodesic-shell.txt" %}}
+{{% include-code-block title="Assume role" file="geodesic/module/usage/examples/assume-role.txt" %}}
+
+## Provision aws-kms
+
+Change directory to `/conf/aws` and run there commands to provision the `kms` backend.
+```bash
+init-terraform
+terraform plan
+terraform apply
+```
+
+{{% include-code-block title="terraform apply" file="secrets-management/examples/terraform-apply-aws-kms.text" %}}
+
 
 # Usage
 
-```
-# chamber KMS config
-ENV CHAMBER_KMS_KEY_ALIAS="alias/example-staging-chamber"
-```
+Use [Chamber]({{< relref "tools/chamber.md" >}}) to manage secrets in KMS.
---
+++ content/secrets-management/examples/terraform-aws-kms.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_version = ">= 0.11.2"
+
+  backend "s3" {}
+}
+
+variable "aws_assume_role_arn" {}
+
+data "aws_caller_identity" "current" {}
+
+variable "tfstate_namespace" {}
+
+variable "tfstate_stage" {}
+
+variable "tfstate_region" {}
+
+
+provider "aws" {
+  assume_role {
+    role_arn = "${var.aws_assume_role_arn}"
+  }
+}
+
+module "chamber_kms_key" {
+  source      = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=tags/0.1.0"
+  namespace   = "${var.tfstate_namespace}"
+  stage       = "${var.tfstate_stage}"
+  name        = "chamber"
+  description = "KMS key for chamber"
+}
+
+# Chamber user for CI/CD systems that cannot leverage IAM instance profiles
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-paramstore-access.html
+module "chamber_user" {
+  source        = "git::https://github.com/cloudposse/terraform-aws-iam-chamber-user.git?ref=tags/0.1.4"
+  namespace     = "${var.tfstate_namespace}"
+  stage         = "${var.tfstate_stage}"
+  name          = "chamber"
+  enabled       = "true"
+  attributes    = ["codefresh"]
+  kms_key_arn   = "${module.chamber_kms_key.key_arn}"
+  ssm_resources = ["${format("arn:aws:ssm:%s:%s:parameter/kops/*", var.tfstate_region, data.aws_caller_identity.current.account_id)}"]
+}
+
+output "chamber_kms_key_arn" {
+  value       = "${module.chamber_kms_key.key_arn}"
+  description = "KMS key ARN"
+}
+
+output "chamber_kms_key_id" {
+  value       = "${module.chamber_kms_key.key_id}"
+  description = "KMS key ID"
+}
+
+output "chamber_kms_key_alias_arn" {
+  value       = "${module.chamber_kms_key.alias_arn}"
+  description = "KMS key alias ARN"
+}
+
+output "chamber_kms_key_alias_name" {
+  value       = "${module.chamber_kms_key.alias_name}"
+  description = "KMS key alias name"
+}
+
+output "chamber_user_name" {
+  value       = "${module.chamber_user.user_name}"
+  description = "Normalized IAM user name"
+}
+
+output "chamber_user_arn" {
+  value       = "${module.chamber_user.user_arn}"
+  description = "The ARN assigned by AWS for the user"
+}
+
+output "chamber_user_unique_id" {
+  value       = "${module.chamber_user.user_unique_id}"
+  description = "The user unique ID assigned by AWS"
+}
+
+output "chamber_access_key_id" {
+  value       = "${module.chamber_user.access_key_id}"
+  description = "The access key ID"
+}
+
+output "chamber_secret_access_key" {
+  value       = "${module.chamber_user.secret_access_key}"
+  description = "The secret access key. This will be written to the state file in plain-text"
+}
