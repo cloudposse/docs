946677e2926b0a89b3e5cca824485c114562fb5f
finnsky <finnsky2010@gmail.com>
2018-07-13 01:55:10 +0600

    fixes according #issuecomment-398864003

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function(){
   $('.page').glossarizer({
     sourceURL: '/glossary.json',
@@ -27,81 +29,78 @@ $(function(){
     searchParameters: {
       hitsPerPage: 5
     },
-    searchFunction(helper) {
+    searchFunction(helper) {      
       if (helper.state.query === '') {
         hideHits();
         return;
       }
-      if(helper.state.query.length === 1) {
+      if (helper.state.query.length === 1) {
         showHits();
       }
       helper.search();
     }
   });
 
+  search.addWidgets(
+    [
+      instantsearch.widgets.stats({
+        container: '#search-stats',
+        templates: {
+          body: "<h3>Search results for \"<em>{{query}}</em>\"</h3><span>(found {{nbHits}} results in {{processingTimeMS}} ms)</span>"
+        }
+      }),
+      instantsearch.widgets.searchBox({
+        container: '#search-box',
+        placeholder: 'Search',
+        poweredBy: false,    
+        placeholder: 'Search',
+        autofocus: true,
+        loadingIndicator: true,
+      }),
+      instantsearch.widgets.hits({
+        container: '#search-hits',
+        //autoHideContainer: true,
+        //collapsible: true,
+        templates: {
+          empty: "We didn't find any results for the search <em>\"{{query}}\"</em>",
+          item: '<a href="{{ url }}"><p class="search-hit-container"><div><strong class="search-hit-title">{{{ _highlightResult.title.value }}}</strong><em class="section">{{{ section }}}</em></div><p class="text-overflow">{{{ _highlightResult.description.value }}}</p><em class="tags">{{{ tags_text }}}</em></div></a>',
+        },
+        showMoreLabel: "Load more results...",
+        transformData: {
+          item: function(data) {
+            // Process tags
+            if(data.tags) {
+              const tags = data.tags.map(function(value) {
+                return '#' + value.toLowerCase().replace(' ', '-')
+              })
+              data.tags_text = tags.join(' ')
+            } else {
+              data.tags_text = ""
+            }
+  
+            // return normalized data
+            return data
+          }
+        }
+      })
+    ]
+  );
 
   function showHits() {
-    //console.log("showHits");
-    searchResults.classList.remove('hidden');
     searchBar.classList.add('active');
+    setTimeout(function() {
+      searchResults.classList.remove('hidden');
+    }, 300);
   }
 
   function hideHits() {
-    //console.log("hideHits");
     searchResults.classList.add('hidden');
     searchBar.classList.remove('active');
+    //To be sure.
+    setTimeout(function() {
+      searchResults.classList.add('hidden');
+    }, 300);
   }
-
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: '#search-hits',
-      //autoHideContainer: true,
-      //collapsible: true,
-      templates: {
-        empty: "We didn't find any results for the search <em>\"{{query}}\"</em>",
-        item: '<a href="{{ url }}"><p class="search-hit-container"><div><strong class="search-hit-title">{{{ _highlightResult.title.value }}}</strong><em class="section">{{{ section }}}</em></div><p class="text-overflow">{{{ _highlightResult.description.value }}}</p><em class="tags">{{{ tags_text }}}</em></div></a>',
-      },
-      showMoreLabel: "Load more results...",
-      transformData: {
-        item: function(data) {
-          // Process tags
-          if(data.tags) {
-            const tags = data.tags.map(function(value) {
-              return '#' + value.toLowerCase().replace(' ', '-')
-            })
-            data.tags_text = tags.join(' ')
-          } else {
-            data.tags_text = ""
-          }
-
-          // return normalized data
-          return data
-        }
-      }
-    })
-  );
-
-  search.addWidget(
-    instantsearch.widgets.stats({
-      container: '#search-stats',
-      templates: {
-        body: "<h3>Search results for \"<em>{{query}}</em>\"</h3><span>(found {{nbHits}} results in {{processingTimeMS}} ms)</span>"
-      }
-    })
-  );
-
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: '#search-box',
-      placeholder: 'Search',
-      poweredBy: false,
-      magnifier: true,
-      placeholder: 'Search',
-      autofocus: true,
-      reset: true,
-      loadingIndicator: true
-    })
-  );
 
   /*
   search.addWidget(
