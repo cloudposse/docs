50e496ddfe56dfcc273e65510ede3511c37d9a47
finnsky <finnsky2010@gmail.com>
2018-07-29 14:06:42 +0600

    Added Chapter Links to Chapter Titles #80

--- themes/hugo-theme-docdock/static/theme-flex/script.js
+++ themes/hugo-theme-docdock/static/theme-flex/script.js
@@ -46,11 +46,10 @@ jQuery(document).ready(function() {
         }
     });
 
-
 	// Clipboard
 	// Add link button for every
     var text, clip = new Clipboard('.anchor');
-    $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function (index, html) {
+    $(".page-content__content :header").append(function (index, html) {
         var element = $(this);
         var url = document.location.origin + document.location.pathname;
         var link = url + "#" + element[0].id;
