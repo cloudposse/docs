fc90ed8709a035573b1451e2d17d5c072c65364a
Alexander Babai <aliaksandr.babai@gmail.com>
2018-11-19 17:06:38 +0300

    refactor(scripts.update-terraform-modules): fetch file-set instead of cloning
    fetche files from raw.github.com via curl instead of full git clone

--- scripts/update-terraform-modules.sh
+++ scripts/update-terraform-modules.sh
@@ -15,7 +15,7 @@ echo "Fetching repository names..."
 if [ ! -f $tmp_dir/modules_cache.txt ]; then
     modules=$(
         seq $github_api_pages | \
-        xargs -I "{}" curl -sSL "https://api.github.com/orgs/${github_org}/repos?per_page=${github_api_limit}&page={}" | \
+        xargs -I "{}" curl --fail -sSL "https://api.github.com/orgs/${github_org}/repos?per_page=${github_api_limit}&page={}" | \
         jq -r '.[] | .name' | \
         sort | grep terraform-*
     )
@@ -24,14 +24,19 @@ else
     modules=$(<$tmp_dir/modules_cache.txt)
 fi
 
-## Clone repositories
-echo "Cloning repositories..."
-mkdir -p $tmp_dir/projects/terraform
+## Fetch terraform-modules content
+echo "Fetching terraform-modules content..."
 for module in $modules
 do
     module_dir=$tmp_dir/projects/terraform/$module
     if [ ! -d $module_dir ]; then
-        git clone https://github.com/$github_org/$module.git $module_dir
+        mkdir -p $module_dir
+        echo ""
+        echo "Fething files for $module..."
+        curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/README.yaml" -o $module_dir/README.yaml
+        mkdir -p $module_dir/docs
+        curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/docs/targets.md" -o $module_dir/docs/targets.md
+        curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/docs/terraform.md" -o $module_dir/docs/terraform.md
     fi
 done
 
@@ -45,6 +50,7 @@ do
     module=$(basename $module_dir)
     module_readme_yaml=$module_dir/README.yaml
     module_page=content/terraform-modules/$module.md
+    
     if [ -f $module_readme_yaml ]; then
         make readme README_TEMPLATE_FILE=$module_template README_FILE=$module_page README_YAML=$module_readme_yaml README_TEMPLATE_YAML=$module_readme_yaml README_INCLUDES=$(pwd)/$module_dir
     fi
