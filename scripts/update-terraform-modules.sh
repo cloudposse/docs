0ff0d6c8ec3061fbe404424a8efba40c3b2c35d8
Alexander Babai <aliaksandr.babai@gmail.com>
2018-11-26 16:05:43 +0300

    refactor(scripts.update-terraform-modules): atomic fetch & render
    * combined code about fetching module's content to the function
    *  combined code about rendering of the module to the function
    * render page imidiately after fetching (fetching is on demand)

--- scripts/update-terraform-modules.sh
+++ scripts/update-terraform-modules.sh
@@ -10,6 +10,37 @@ github_api_pages=3
 
 modules=[]
 
+function fetch_module {
+    local module=$1
+    local module_dir=$2
+
+    mkdir -p $module_dir
+    echo ""
+    echo "Fething files for $module..."
+    curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/README.yaml" -o $module_dir/README.yaml
+    mkdir -p $module_dir/docs
+    curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/docs/targets.md" -o $module_dir/docs/targets.md
+    curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/docs/terraform.md" -o $module_dir/docs/terraform.md
+}
+
+function render_module {
+    local module=$1
+    local module_dir=$2
+    local module_template=templates/terraform-module-page.md
+    local module_readme_yaml=$module_dir/README.yaml
+    local module_page=content/terraform-modules/$module.md
+    
+    if [ -f $module_readme_yaml ]; then
+        make readme README_TEMPLATE_FILE=$module_template README_FILE=$module_page README_YAML=$module_readme_yaml README_TEMPLATE_YAML=$module_readme_yaml README_INCLUDES=$(pwd)/$module_dir
+    fi
+}
+
+
+## Install dependencies
+echo "Installing dependencies..."
+# make init
+make readme/deps
+
 ## Fetch repository names
 echo "Fetching repository names..."
 if [ ! -f $tmp_dir/modules_cache.txt ]; then
@@ -24,36 +55,15 @@ else
     modules=$(<$tmp_dir/modules_cache.txt)
 fi
 
-## Fetch terraform-modules content
-echo "Fetching terraform-modules content..."
+## Process terraform-modules content
+echo "Processing terraform-modules..."
 for module in $modules
 do
     module_dir=$tmp_dir/projects/terraform/$module
     if [ ! -d $module_dir ]; then
-        mkdir -p $module_dir
-        echo ""
-        echo "Fething files for $module..."
-        curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/README.yaml" -o $module_dir/README.yaml
-        mkdir -p $module_dir/docs
-        curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/docs/targets.md" -o $module_dir/docs/targets.md
-        curl --fail -sSL --write-out "%{url_effective}\n" "https://raw.githubusercontent.com/${github_org}/${module}/master/docs/terraform.md" -o $module_dir/docs/terraform.md
+        fetch_module $module $module_dir
     fi
-done
-
-## Render pages
-echo "Rendering pages..."
-# make init
-make readme/deps
-module_template=templates/terraform-module-page.md
-for module_dir in $tmp_dir/projects/terraform/*
-do
-    module=$(basename $module_dir)
-    module_readme_yaml=$module_dir/README.yaml
-    module_page=content/terraform-modules/$module.md
-    
-    if [ -f $module_readme_yaml ]; then
-        make readme README_TEMPLATE_FILE=$module_template README_FILE=$module_page README_YAML=$module_readme_yaml README_TEMPLATE_YAML=$module_readme_yaml README_INCLUDES=$(pwd)/$module_dir
-    fi
+    render_module $module $module_dir
 done
 
 ## Delete temp data
