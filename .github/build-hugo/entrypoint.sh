cf8e8ecf00ab8ed73833857e78e4291baa31edf9
Dylan Bannon <dylanbannon5@gmail.com>
2021-01-27 17:47:32 -0800

    debug

--- .github/build-hugo/entrypoint.sh
+++ .github/build-hugo/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-apt-get update && \
-    apt-get install -y python3 python3-pip locales jq && \
-    rm -rf /var/lib/apt/lists/* && \
-    localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 && \
-    npm install -g atomic-algolia@0.3.15 cloudflare-cli@3.0.0 && \
-	pip3 install asciinema
+#apt-get update && \
+#    apt-get install -y python3 python3-pip locales jq && \
+#    rm -rf /var/lib/apt/lists/* && \
+#    localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 && \
+#    npm install -g atomic-algolia@0.3.15 cloudflare-cli@3.0.0 && \
+#	pip3 install asciinema
 
 set +e -x
 make lint
