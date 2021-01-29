7794ade019a22a7d10235b7050b3fc5e1cd3996e
Dylan Bannon <dylanbannon5@gmail.com>
2021-01-28 21:59:05 -0800

    debug

--- .github/build-versioned-hugo/entrypoint.sh
+++ .github/build-versioned-hugo/entrypoint.sh
@@ -5,7 +5,7 @@ make git/show
 make git/export | tee -a $(pwd)/env_vars_to_export
 make semver/show
 make semver/export | tee -a $(pwd)/env_vars_to_export
-if "${SEMVERSION_TAG}" != ''; do
+if "${SEMVERSION_TAG}" != ''; then
   export HUGO_URL=${HUGO_URL}release/${SEMVERSION_TAG}
   export HUGO_PUBLISH_DIR=public/release/${SEMVERSION_TAG}
   make lint
