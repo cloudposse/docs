2ddac9113174ed273e708c94a7e6d061cfb6edcc
Dylan Bannon <dylanbannon5@gmail.com>
2021-01-28 21:24:53 -0800

    last commit worked, so further debugging

---
+++ .github/build-versioned-hugo/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set +e -x
+make git/show
+make git/export | tee -a $(pwd)/env_vars_to_export
+make semver/show
+make semver/export | tee -a $(pwd)/env_vars_to_export
+if "${SEMVERSION_TAG}" != ''; do
+  export HUGO_URL=${HUGO_URL}release/${SEMVERSION_TAG}
+  export HUGO_PUBLISH_DIR=public/release/${SEMVERSION_TAG}
+  make lint
+  make release
+  make real-clean hugo/build
+fi
