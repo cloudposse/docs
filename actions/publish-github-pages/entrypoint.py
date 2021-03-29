4bcf1b3837c9827b140bd23005730a8aa0972875
Dylan Bannon <dylanbannon5@gmail.com>
2021-03-28 23:50:27 -0500

    cleaning up and adding whitespace to entrypoint.py

--- actions/publish-github-pages/entrypoint.py
+++ actions/publish-github-pages/entrypoint.py
@@ -49,17 +49,21 @@ def main():
     content_folders = CONTENT.split(",")
     for content_folder in content_folders:
         content_base_path = os.path.join(GITHUB_PAGES_PULL_PATH, content_folder)
+
         # Rename and rearrange content files as needed.
         rearrange_files(content_base_path)
+
         # Now that all .md files have been renamed and rearranged appropriately,
         # collate the docs (.md pages) inside the STAGING_DIR.
         for root, dirs, files in os.walk( content_base_path, topdown=False ):
+
             # If this is a top-level dir (i.e., it is listed directly in CONTENT) and it has no subdirs,
             # every .md file in this folder becomes the basis for a top-level object.
             if root == content_base_path and not len(dirs):
                 markdown_files = [potential_md_file for potential_md_file in files if ".md" in potential_md_file]
                 for markdown_file in markdown_files:
                     promote_markdown_file(markdown_file, root)
+
             # Otherwise, we're gonna copy everything over and preserve the existing file heirarchy.
             else:
                 markdown_files = [potential_md_file for potential_md_file in files if ".md" in potential_md_file]
@@ -136,6 +140,7 @@ def read_in_env_vars():
     STAGING_DIR = STAGING_DIR.rstrip("/")
 
 def checkout_repos():
+
     # Check out:
     # 1) Essential Hugo build tools
     Repo.clone_from(HUGO_REPO, GITHUB_PAGES_HUGO_PATH)
@@ -147,11 +152,13 @@ def checkout_repos():
     Repo.clone_from(GITHUB_PAGES_REPO, GITHUB_PAGES_PUSH_PATH, branch=GITHUB_PAGES_PUSH_BRANCH)
 
 def rearrange_files(content_base_path):
+
     # rename all `README.md` to `_index.md` (hugo convention)
     for root, dirs, files in os.walk(content_base_path):
         for local_file in files:
             if local_file=="README.md":
                 os.rename( os.path.join(root, local_file), os.path.join(root, "_index.md") )
+
     # folders with no subfolders, and only a single md file (usually `_index.md`): `mv foobar/_index.md foobar.md`
     hierarchy_modified = True # init value
     while hierarchy_modified:
@@ -170,13 +177,16 @@ def rearrange_files(content_base_path):
                         hierarchy_modified = True
 
 def insert_frontmatter(file_path, weight=1):
-    # check for frontmatter
+
+    # check for frontmatter in this file
     frontmatter_flag = True
     with open(file_path, "r") as markdown_file:
         line = markdown_file.readline()
         if not re.match("^---", line):
             frontmatter_flag = False
-    # if it's not found, create it by finding the markdown header as frontmatter
+
+    # if the frontmatter wasn't found, create it by finding the markdown header
+    # and using it as the core of the frontmatter
     if not frontmatter_flag:
         title = ""
         with open(file_path, "r") as markdown_file:
@@ -185,6 +195,7 @@ def insert_frontmatter(file_path, weight=1):
                 if match_results:
                     title = match_results.group(1)
                     break
+
         # write out file with frontmatter
         with open(file_path, "r") as markdown_file:
             input_file = markdown_file.read()
@@ -200,14 +211,17 @@ def insert_frontmatter(file_path, weight=1):
             markdown_file.write(input_file)
 
 def promote_markdown_file(markdown_file, root):
+
     # create a folder for the markdown file
     markdown_basename = os.path.splitext(markdown_file)[0]
     os.mkdir( os.path.join(STAGING_DIR, "content", markdown_basename) )
+
     # move the markdown file into the folder
     origin_path = os.path.join(root, markdown_file)
     destination_path = os.path.join(STAGING_DIR, "content", markdown_basename, markdown_file)
     insert_frontmatter(origin_path)
     os.renames( origin_path, destination_path )
+
     # create _index.md file for the folder
     index_string = "---\n" + \
                    f"title: \"{markdown_basename.capitalize()}\"\n" + \
@@ -218,15 +232,6 @@ def promote_markdown_file(markdown_file, root):
     index_path = os.path.join(STAGING_DIR, "content", markdown_basename, "_index.md")
     with open(index_path, "w")as index_file:
         index_file.write(index_string)
-
-def print_file_tree(rootDir):
-    list_dirs = os.walk(rootDir)
-    for root, dirs, files in list_dirs:
-        print(f"root: {root}")
-        for d in dirs:
-            print(os.path.join(root, d))
-        for f in files:
-            print(os.path.join(root, f))
 
 if __name__=="__main__":
     main()
