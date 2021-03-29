2edc7474d2f44fd2c319aafa32d4bd2db3ce9acc
dylanbannon <dylanbannon5@gmail.com>
2021-03-28 20:25:51 -0500

    Update actions/publish-github-pages/entrypoint.py
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- actions/publish-github-pages/entrypoint.py
+++ actions/publish-github-pages/entrypoint.py
@@ -6,7 +6,7 @@
 # GITHUB_PAGES_DIRECTORY - the directory to write the rendered website files to
 # GITHUB_PAGES_REPO - repo containing documentation to be deployed to GitHub Pages
 # GITHUB_PAGES_PULL_BRANCH - the branch of the repo which contains the documentation
-# GITHUB_PAGES_PUSH_BRANCH - the branch of the customer's repo which GitHub Pages will deploy from
+# GITHUB_PAGES_PUSH_BRANCH - the branch of the repo which GitHub Pages will deploy to
 # CONTENT - comma-separated list of directories in the top level of the customer's repo that contain documentation
 # HUGO_URL - URL of the Hugo site after deployment
 # HUGO_PUBLISH_DIR - directory in the customer's repo that GitHub Pages will deploy from
