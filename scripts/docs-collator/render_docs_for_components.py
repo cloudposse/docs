4f4d6176287604a8b9b87136c772396036b91f96
Zinovii Dmytriv <zinovii@gmail.com>
2023-02-03 17:12:19 +0200

    tuned components rendering script

--- scripts/docs-collator/render_docs_for_components.py
+++ scripts/docs-collator/render_docs_for_components.py
@@ -16,82 +16,56 @@ GITHUB_ORG = 'cloudposse'
 GITHUB_REPO = 'terraform-aws-components'
 
 
-def render_index_category(index_category_template, output_dir, subdirs):
-    if len(subdirs) <= 1:
-        return  # nothing to create
-
-    middle_dirs = subdirs[:-1]  # need to skip last dir
-
-    current_path = output_dir
-
-    for subdir in middle_dirs:
-        new_path = os.path.join(current_path, subdir)
-        json_path = os.path.join(new_path, CATEGORY_JSON_FILE)
-        current_path = new_path
-
-        if not os.path.exists(json_path):
-            json_content = index_category_template.render(label=subdir,
-                                                          title=subdir,
-                                                          description=subdir)
-            io.save_string_to_file(json_path, json_content)
-
-
-def render_doc(doc_template, module, source_file, output_dir, subdirs, github_repository):
-    content = io.read_file_to_string(source_file)
+def render_doc(doc_template, module, file, module_download_dir, docs_dir, github_repository):
+    content = io.read_file_to_string(file)
     content = rendering.fix_self_non_closing_br_tags(content)
     content = rendering.fix_custom_non_self_closing_tags_in_pre(content)
     content = rendering.remove_logo_from_the_bottom(content)
-    final_dir = os.path.join(*([output_dir] + subdirs))
-    result_path = os.path.join(final_dir, README_FILE_NAME)
-    tags = ['terraform'] + module.split("-")
+    relative_path = os.path.relpath(file, module_download_dir)
+    result_file = os.path.join(docs_dir, os.path.relpath(file, module_download_dir))  # <module-name>/README.md
 
-    doc_content = doc_template.render(label=module,
-                                      title=module,
-                                      description=module,
+    label = module
+    title = module
+    description = module
+    github_edit_url = f"https://github.com/{github_repository}/edit/master/modules/{relative_path}"
+
+    if '/modules/' in result_file:  # this is submodule
+        submodule_name = os.path.basename(os.path.dirname(result_file))
+
+        label = submodule_name
+        title = submodule_name
+        description = submodule_name
+
+        # renaming final file <module-name>/<module-name>.md
+        result_file = os.path.join(os.path.dirname(result_file), f"{submodule_name}.md")
+
+    io.create_dirs(os.path.dirname(result_file))
+
+    tags = ['terraform', 'aws', module]
+
+    doc_content = doc_template.render(label=label,
+                                      title=title,
+                                      description=description,
                                       content=content,
                                       github_repository=github_repository,
-                                      module=module,
+                                      github_edit_url=github_edit_url,
                                       tags=tags)
 
-    io.save_string_to_file(result_path, doc_content)
+    io.save_string_to_file(result_file, doc_content)
 
 
-def process_module(module, input_dir, output_dir, github_repository):
+def process_module(module, download_dir, docs_dir, github_repository, doc_template):
     logging.info(f"Processing module: {module}")
 
-    output_component_dir = os.path.join(output_dir, module)
-    module_dir = os.path.join(input_dir, module)
+    module_download_dir = os.path.join(download_dir, module)
 
-    io.remove_dir(output_component_dir)
-
-    files = io.get_filenames_in_dir(module_dir, README_FILE_NAME, True)
-
-    jenv = templating.init_templating(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates'))
-    index_category_template = jenv.get_template(INDEX_CATEGORY_TEMPLATE_FILE)
-    doc_template = jenv.get_template(COMPONENT_README_TEMPLATE_FILE)
-
-    # create subdirs
-    for file in files:
-        subdir = os.path.dirname(file)
-        doc_dir = os.path.join(output_dir, subdir[len(input_dir) + 1:])
-        io.create_dirs(doc_dir)
+    files = io.get_filenames_in_dir(module_download_dir, README_FILE_NAME, True)
 
     for file in files:
-        items = file.split(os.sep)
-        items = items[len(input_dir.split(os.sep)):]  # remove input dir
-        subdirs = items[:-1]  # get all dir
-
-        render_index_category(index_category_template, output_dir, subdirs)
-
-        doc_name = module
-
-        if len(subdirs) != 0:
-            doc_name = subdirs[-1]
-
-        render_doc(doc_template, doc_name, file, output_dir, subdirs, github_repository)
+        render_doc(doc_template, module, file, download_dir, docs_dir, github_repository)
 
 
-def main(input_dir, output_dir, github_repo, modules_to_skip):
+def main(input_dir, output_dir, github_repo):
     modules_dir = os.path.join(input_dir, 'modules')
 
     logging.info(f"Looking for modules in: {modules_dir}")
@@ -100,28 +74,23 @@ def main(input_dir, output_dir, github_repo, modules_to_skip):
 
     logging.info(f"Found {len(modules)} modules")
 
-    skip_modules = set([skip.strip() for skip in modules_to_skip.split(",")])
+    jenv = templating.init_templating(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates'))
+    doc_template = jenv.get_template(COMPONENT_README_TEMPLATE_FILE)
 
     for module in modules:
-        if module in skip_modules:
-            logging.info(f"Module '{module}' in skip list")
-            continue
-
-        process_module(module, modules_dir, output_dir, github_repo)
+        process_module(module, modules_dir, output_dir, github_repo, doc_template)
 
 
 @click.command()
 @click.option('--input-dir', default=CLONED_REPO_DIR, required=True, help="Path to cloned repo")
 @click.option('--output-dir', default=OUTPUT_DOC_DIR, required=False, help="Rendered component output dir")
-@click.option('--github-org', default=GITHUB_ORG, required=True, help="Github org name")
-@click.option('--modules-to-skip', default='', required=False, help="CSV list of modules to skip")
 @click.option('--log-level', default=False, required=False, help="Recursive lookup in sub folders of README.md files")
-def cli_main(input_dir, output_dir, github_org, modules_to_skip, log_level):
+def cli_main(input_dir, output_dir, log_level):
     logging.basicConfig(format='[%(asctime)s] %(levelname)s %(message)s',
                         datefmt='%d-%m-%Y %H:%M:%S',
                         level=logging.getLevelName(log_level))
 
-    main(input_dir, output_dir, f"{github_org}/{GITHUB_REPO}", modules_to_skip)
+    main(input_dir, output_dir, f"{GITHUB_ORG}/{GITHUB_REPO}")
 
 
 if __name__ == "__main__":
--- scripts/docs-collator/templates/component.readme.md
+++ scripts/docs-collator/templates/component.readme.md
@@ -3,7 +3,7 @@ title: {{ title }}
 sidebar_label: {{ label }}
 sidebar_class_name: command
 description: {{ description }}
-custom_edit_url: https://github.com/{{ github_repository }}/edit/master/modules/{{ module }}/README.md
+custom_edit_url: {{ github_edit_url }}
 tags: [{{ tags|join(', ') }}]
 ---
 
