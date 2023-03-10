7b6c7b1608864d757600331b542a020faad0c955
Zinovii Dmytriv <zinovii@gmail.com>
2023-03-10 17:39:41 +0200

    update

--- algolia/reindex.sh
+++ algolia/reindex.sh
@@ -13,6 +13,7 @@ cat algolia/template.json \
   | jq '.index_name="'${ALGOLIA_INDEX_NAME}'"' \
   | jq '.start_urls[0]="https://'${DEPLOYMENT_HOST}'/"' \
   | jq '.sitemap_urls[0]="https://'${DEPLOYMENT_HOST}'/sitemap.xml"' \
+  | jq '.sitemap_urls[1]="https://'${DEPLOYMENT_HOST}'/reference-architecture/sitemap.xml"' \
   > algolia.index.json
 
 cat algolia.index.json
