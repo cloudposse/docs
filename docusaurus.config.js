6d3b604b9073c1a78be3e19438b5eab157d3e633
milldr <miller0daniel@gmail.com>
2024-08-12 16:22:48 -0400

    enforce links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
   url: 'https://docs.cloudposse.com',
   baseUrl: '/',
   trailingSlash: true,
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   onDuplicateRoutes: 'warn',
   favicon: 'img/favicon.png',
