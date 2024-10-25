1af4d44ce86ca707f80b074a9e237ab91ee83288
Igor Rodionov <goruha@gmail.com>
2024-10-25 12:40:58 +0200

    Fix rendering readme'

--- scripts/docs-collator/utils/rendering.py
+++ scripts/docs-collator/utils/rendering.py
@@ -1,3 +1,4 @@
+import os
 import re
 import subprocess
 import yaml
@@ -134,11 +135,11 @@ def render_terraform_docs(terraform_module_path, terraform_docs_configuration):
         command = [
             "mkdir",
             "-p",
-            "./docs"
+            os.path.join(terraform_module_path, "docs")
         ]
 
         # Run the command and capture the output
-        return subprocess.run(command, check=True, capture_output=True, text=True)
+        subprocess.run(command, check=True, capture_output=True, text=True)
 
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running creating docs dir: {e.stderr}")
