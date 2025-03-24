3f10265d7389f1ccc424e99e02df1af192c7ecc8
milldr <miller0daniel@gmail.com>
2025-03-24 10:33:43 -0400

    reset changes

--- src/components/AnnouncementSection/mdx.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import AnnouncementSection from './index';
-
-export default function MDXAnnouncementSection() {
-  const {siteConfig} = useDocusaurusContext();
-  const announcements = siteConfig.customFields?.announcements || [];
-  return <AnnouncementSection announcements={announcements} />;
-} 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,12 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Hero from "../theme/sections/hero/hero";
 import HomeCallouts from "../theme/sections/home-callouts/home-callouts";
-import AnnouncementSection from "../components/AnnouncementSection";
-import { formatDate } from '../utils/date';
 import "./index.css"
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   const title = `${siteConfig.title}`
   const tagline = `${siteConfig.tagline}`
-  
-  // Get announcements from siteConfig.customFields
-  const announcements = siteConfig.customFields?.announcements || [];
-
   return (
     <div className="landing-page">
       <Layout
@@ -22,7 +16,6 @@ export default function Home() {
         <main>
           <Hero title={title} />
           <HomeCallouts />
-          <AnnouncementSection announcements={announcements} />
         </main>
       </Layout>
     </div>
