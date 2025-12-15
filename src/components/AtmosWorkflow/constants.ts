d1772c6b552e97742956a930fa70db20dc5c119a
milldr <miller0daniel@gmail.com>
2025-12-15 15:18:22 -0500

    fix: point AtmosWorkflow component to master branch instead of stale feature branch

--- src/components/AtmosWorkflow/constants.ts
+++ src/components/AtmosWorkflow/constants.ts
@@ -1,5 +1,4 @@
 // constants.ts
 
-// TODO: Change back to 'master' after merging https://github.com/cloudposse/refarch-scaffold/pull/818
-export const CLOUDPOSSE_DOCS_URL = 'https://raw.githubusercontent.com/cloudposse/docs/upstream-account-map-less/';
+export const CLOUDPOSSE_DOCS_URL = 'https://raw.githubusercontent.com/cloudposse/docs/master/';
 export const WORKFLOWS_DIRECTORY_PATH = 'examples/snippets/stacks/workflows/';
